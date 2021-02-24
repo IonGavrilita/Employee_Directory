5eb278842c669fa521c29fac7aef2870c9a77114
Ion Gavrilita <gavrilita.ion.93@gmail.com>
2021-02-23 16:42:05 -0800

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+  
+import React from 'react'
+import './style.css'
+
+function Footer(){
+    return (
+        <footer>
+            <ul className="footer">
+                <li>Copyright © Ion Gavrilita</li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
